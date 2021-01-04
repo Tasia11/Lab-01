af26b2743514d7a5413217ba299f57e11652002f
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-05 00:03:17 +0300

    Update student.hpp

--- include/student.hpp
+++ include/student.hpp
@@ -3,29 +3,28 @@
 
 #include <any>
 #include <exception>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <nlohmann/json.hpp>
 #include <string>
 
-using json = nlohmann::json;
+using nlohmann::json;
+
 class Student {
  public:
-  explicit Student(const json &obj);
-  Student();
-  [[nodiscard]] const std::string &getName() const;
-  void setName(const json &name);
-  [[nodiscard]] const std::any &getGroup() const;
-  void setGroup(const json &group);
-  [[nodiscard]] double getAvg() const;
-  void setAvg(const json &avg);
-  [[nodiscard]] const std::any &getDebt() const;
-  void setDebt(const json &debt);
-  void from_json(const json &j);
+  explicit Student(const json& js);
+  const std::string& getName() const;
+  const std::any& getGroup() const;
+  const std::any& getAvg() const;
+  const std::any& getDebt() const;
 
  private:
   std::string Name;
   std::any Group;
-  double Avg = 0;
+  std::any Avg;
   std::any Debt;
 };
+
 
 #endif  // INCLUDE_STUDENT_HPP_
